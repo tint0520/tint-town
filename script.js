c6007644545bba6c480a61edb962745e1de9a63b
tint0520 <tint.collect@gmail.com>
2025-04-05 22:49:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let markers = [];
 let currentLayer = "town";
 
 const SHEET_ID = '1IapOBmEDnMok0a1qhwGhusfs9Li2AOdhvfCM_VF7c8Y';  // 👈 你之後換掉
-const SHEET_NAME = "表單回應 1";
+const SHEET_NAME = "Tint.Maps";
 const SHEET_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json`;
 
 function initMap() {
